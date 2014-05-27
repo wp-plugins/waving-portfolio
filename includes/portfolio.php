8d18b47a6d73008885952b6560d0705f2816230e
aelbuni <aelbuni@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-27 04:41:23 +0000

    * Add category support for portfolio items, where you can tag portfolio with categories to be ordered later for your customers accordingly.
    git-svn-id: https://plugins.svn.wordpress.org/waving-portfolio/tags/1.0.5@921445 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/portfolio.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Waving Portfolio Controller
+ *
+ * @package   waving_portfolio
+ * @author    Abdulrhman Elbuni
+ * @license   GPL-2.0+
+ * @link      http://www.itechflare.com/
+ * @copyright 2014-2015 iTechFlare
+ *
+ * @wordpress-plugin
+ * Plugin Name: Waving Portfolio
+ * Plugin URI:  http://www.itechflare.com/
+ * Description: Enables a portfolio post type and taxonomies.
+ * Version:     1.0.1
+ * Author:      Abdulrhman Elbuni
+ * Author URI:  http://www.itechflare.com/
+ * Text Domain: portfolioposttype
+ * License:     GPL-2.0+
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ * Domain Path: /languages
+ */
+
+class Portfolio {
+
+    public $image;
+    public $content;
+    public $title;
+    public $date;
+    
+    public function __construct( ) {
+
+    }
+}
